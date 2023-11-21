0d8c60769d47f41be9a50e1b6862e046ae2fdc33
LD-Tien <leductien00@gmail.com>
2023-11-21 17:21:56 +0700

    Update UI: Orders, Account info

--- app/Http/Livewire/User/UserDashBoardComponent.php
+++ app/Http/Livewire/User/UserDashBoardComponent.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class UserDashBoardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.user.user-dash-board-component');
+        $orders = Order::where('user_id', Auth::user()->id)->orderBy("created_at", "desc")->paginate(5);
+        return view('livewire.user.user-dash-board-component', ['orders' => $orders]);
     }
 }
