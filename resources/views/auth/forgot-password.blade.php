6b0ef9f95b8641c75e7de4862d91e26485f451c2
LD-Tien <leductien00@gmail.com>
2023-11-22 10:46:34 +0700

    Update UI forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,66 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<x-app-layout>
+    <main class="main">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="/" rel="nofollow">Trang chủ</a>
+                    <span></span> Quên mật khẩu
+                </div>
+            </div>
         </div>
+        <section class="pt-150 pb-150">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10 m-auto">
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <div
+                                    class="login_wrap widget-taber-content p-30 background-white border-radius-10 mb-md-5 mb-lg-0 mb-sm-5">
+                                    <div class="padding_eight_all bg-white">
+                                        <div class="heading_s1">
+                                            <h3 class="mb-30">Nhập email tài khoản của bạn</h3>
+                                        </div>
+                                        @if (session('status'))
+                                        <div class="alert alert-success mt-3 mb-3">
+                                            {{ session('status') }}
+                                        </div>
+                                        @endif
+                                        @if ($errors->any())
+                                        <div class="alert alert-danger mt-3 mb-3">
+                                            <ul>
+                                                @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                        @endif
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+                                        <form method="POST" action="{{ route('password.email') }}">
+                                            @csrf
+
+                                            <div class="block">
+                                                <x-label for="email" value="{{ __('Email') }}" />
+                                                <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                                    :value="old('email')" required autofocus autocomplete="username" />
+                                            </div>
+
+                                            <div class="flex items-center justify-end mt-4">
+                                                <x-button>
+                                                    {{ __('Gửi yêu cầu cấp lại mật khẩu') }}
+                                                </x-button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-1"></div>
+                            <div class="col-lg-6">
+                                <img src="assets/imgs/login.png">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
-
-        <x-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+        </section>
+    </main>
+</x-app-layout>
